a0a0963a9e249832d37271aef710b7756c65095c
JongHan Leem <jleem42@gmail.com>
2024-03-14 22:22:12 +0900

    feat: add softmax

--- assignments/assignment1/cs231n/classifiers/softmax.py
+++ assignments/assignment1/cs231n/classifiers/softmax.py
@@ -34,7 +34,53 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    (D, C) = W.shape
+    (N, _) = X.shape
+
+    # ** FORWARD PASS ** #
+    scores = X @ W  # (N, C)
+    cross_entropies = np.zeros((N))
+
+    for sample_index in range(N):
+        y_true = y[sample_index]
+        sample_scores = scores[sample_index]
+        score_true = sample_scores[y_true]
+
+        cross_entropy = -score_true + np.log(np.sum(np.exp(sample_scores)))
+        cross_entropies[sample_index] = cross_entropy
+
+    loss = np.mean(cross_entropies) + reg * np.sum(W ** 2)
+
+    # ** BACKWARD PASS ** #
+    d_loss = 1  # (1, )
+
+    # Backprop: loss = np.mean(cross_entropies) + ...
+    d_cross_entropies = d_loss * np.full_like(cross_entropies, 1 / N)  # (N, )
+    # Backprop: loss = ... + reg * np.sum(W ** 2)
+    dW += d_loss * reg * 2 * W  # (D, C)
+
+    d_scores = np.zeros_like(scores)  # (N, C)
+
+    for sample_index in range(N):
+        y_true = y[sample_index]
+
+        # Backprop: cross_entropies[sample_index] = cross_entropy
+        d_cross_entropy = d_cross_entropies[sample_index]  # (1, )
+
+        d_sample_scores = np.zeros_like(sample_scores)  # (C, )
+        # Backprop: cross_entropy = -score_true + ...
+        d_score_true = -d_cross_entropy  # (1, )
+        d_sample_scores[y_true] += d_score_true
+
+        # Backprop: cross_entropy = ... + np.log(np.sum(np.exp(sample_scores)))
+        sample_scores = scores[sample_index]
+        sample_scores_exp = np.exp(sample_scores)
+        d_sample_scores += d_cross_entropy * sample_scores_exp / np.sum(sample_scores_exp)
+
+        d_scores[sample_index] = d_sample_scores
+
+    # Backprop: scores = X @ W
+    dW += X.T @ d_scores
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -59,7 +105,36 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    (D, C) = W.shape
+    (N, _) = X.shape
+
+    # ** FORWARD PASS ** #
+
+    scores = X @ W  # (N, C)
+    scores_true = scores[range(N), y]  # (N, )
+    scores_exp = np.exp(scores)
+    scores_exp_sum = np.sum(scores_exp, axis=1)
+    LSE = np.log(scores_exp_sum)  # (N, )
+    cross_entropies = -scores_true + LSE  # (N, )
+    loss = np.mean(cross_entropies)
+
+    # ** BACKWARD PASS ** #
+
+    # Backprop: loss = np.mean(cross_entropies)
+    d_cross_entropies = np.full_like(cross_entropies, 1 / N)  # (N, )
+
+    # Backprop: cross_entropies = -scores_true + LSE
+    d_scores_true = -d_cross_entropies  # (N, )
+    d_LSE = d_cross_entropies  # (N, )
+
+    d_scores = np.zeros_like(scores)  # (N, C)
+    # Backprop: LSE = np.log(np.sum(np.exp(scores), axis=1))
+    d_scores += d_LSE[:, np.newaxis] * (scores_exp / scores_exp_sum[:, np.newaxis])
+    # Backprop: scores_true = scores[range(N), y]
+    d_scores[range(N), y] += d_scores_true
+
+    # Backprop: scores = X @ W
+    dW = X.T @ d_scores
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
